1a9bdc6a2c795e878ffa1a0d95e821f341240c4f
Ojas <ojasbendale18200@gmail.com>
2023-11-01 22:25:06 +0530

    setup navbar and sidebar

---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { BarChart, Compass, Layout, List } from "lucide-react";
+import SidebarItems from "./sidebar-items";
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Browse",
+    href: "/search",
+  },
+];
+const SidebarRoutes = () => {
+  const routes = guestRoutes;
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItems
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SidebarRoutes;
